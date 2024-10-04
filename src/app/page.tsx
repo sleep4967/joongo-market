a2d21910e49a5ee1a779898839b68c9d0de8d74c
Kimmingyu <sleep4967>
2024-10-04 15:24:40 +0900

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+function HomePage() {
+  return (
+    <div></div>
+  );
+}
+
+export default HomePage
+
+// git status
+// git add .
+// git commit -m "chore: remove unnecessary codes"
+// git push
+``
